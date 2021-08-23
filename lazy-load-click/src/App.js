6e5775fe88c1d24dd38db6b4d9ffbce2616b836b
Trevor Pierce <pierce.trevor@gmail.com>
2021-08-23 08:02:28 -0500

    Adding a README and final helper markup.

--- lazy-load-click/src/App.js
+++ lazy-load-click/src/App.js
@@ -9,14 +9,28 @@ const App = () => {
   const toggleVisible = () => setVisible(!isVisible);
 
   return (
-    <>
+    <main>
+      <h1>Click to lazy load an important message</h1>
+      <p>
+        Open your Developer Tools, and select the <strong>Network</strong> tab.
+        When you click the "Load" button, you should see a new Webpack chunk
+        added to the list of items. This is how you'll know the React.lazy load
+        is working correctly.
+      </p>
+      <p>
+        Enable{" "}
+        <a href="https://css-tricks.com/throttling-the-network/">
+          network throttling
+        </a>{" "}
+        to see the Suspense fallback behavior.
+      </p>
       <Button toggleVisible={toggleVisible} />
       {isVisible && (
         <Suspense fallback={<p>Loading...</p>}>
           <Message />
         </Suspense>
       )}
-    </>
+    </main>
   );
 };
 
--- lazy-load-click/src/components/message/Message.js
+++ lazy-load-click/src/components/message/Message.js
@@ -4,8 +4,8 @@ const Message = (props) => {
       <h2>This is a message box</h2>
       <p>
         In the event of an actual message, this box would contain more useful
-        information. It could be something like a news ticker, stock quotes, or
-        other data that would appear based on asynchronous events.
+        information. It could be something like a modal dialog, error pane, or
+        other useful message that is determined by user action.
       </p>
     </div>
   );
