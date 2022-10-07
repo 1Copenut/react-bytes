342d424440938563e36a188ec06d0ec25ee97105
1Copenut <pierce.trevor@gmail.com>
2022-10-07 16:40:00 -0500

    First commit before adding dependencies.

---
+++ react-query/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>React Query API</h1>
+      </header>
+      <main>
+        <div>This will be the main content area. Plain, I know.</div>
+      </main>
+    </>
+  );
+};
+
+export default App;
