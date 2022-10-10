7b7225d6f220fa2f7a3af00eee86b0473fac39c3
1Copenut <trevor.pierce@elastic.co>
2022-10-10 09:20:59 -0500

    Finished the useBoolean custom hook implementation.

--- custom-hooks/src/components/ButtonBlock/ButtonBlock.js
+++ custom-hooks/src/components/ButtonBlock/ButtonBlock.js
@@ -1,19 +1,34 @@
 import React from "react";
-import useBoolean from "../../hooks/useBoolean";
 import styles from "./ButtonBlock.module.css";
+import useBoolean from "../../hooks/useBoolean";
 
-const ButtonBlock = () => {
-  const [toggle, { setToggle }] = useBoolean(false);
+const ButtonBlock = (props) => {
+  const { isToggle } = props;
+  const [toggle, { setToggle, setTrue, setFalse }] = useBoolean(false);
   const handleToggle = () => setToggle(!toggle);
+
+  if (isToggle) {
+    return (
+      <div className={styles.container}>
+        <button
+          className={styles.toggleButton}
+          type="button"
+          onClick={handleToggle}
+        >
+          Toggle now!
+        </button>
+        <span className={styles.toggleStatus}>{toggle.toString()}</span>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.container}>
-      <button
-        className={styles.toggleButton}
-        type="button"
-        onClick={handleToggle}
-      >
-        Toggle now!
+      <button className={styles.toggleButton} type="button" onClick={setTrue}>
+        It was always true!
+      </button>
+      <button className={styles.toggleButton} type="button" onClick={setFalse}>
+        No, you are wrong.
       </button>
       <span className={styles.toggleStatus}>{toggle.toString()}</span>
     </div>
