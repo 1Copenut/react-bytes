c614ef4faeed7199c0b6b54d3c64cde3ef270db9
1Copenut <trevor.pierce@elastic.co>
2023-12-11 10:18:57 -0600

    First Dropdown component before refactoring.

--- headless-dropdown-component/src/App.tsx
+++ headless-dropdown-component/src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import { Dropdown, DropdownItem } from "./Components/Dropdown/Dropdown";
 
-function App() {
+const items: DropdownItem[] = [
+  { text: "First item", description: "This is the first non-original item" },
+  { text: "Second item", description: "This is the second non-original item" },
+];
+
+const App = () => {
   return (
     <main>
       <h1>Headless dropdown component</h1>
+      <div>
+        <Dropdown items={items} />
+      </div>
     </main>
   );
-}
+};
 
 export default App;
---
+++ headless-dropdown-component/src/Components/Dropdown/Dropdown.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+interface DropdownItem {
+  icon?: string;
+  text: string;
+  description: string;
+}
+
+type DropdownProps = {
+  items: DropdownItem[];
+};
+
+const Dropdown = ({ items }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<DropdownItem | null>(null);
+
+  return (
+    <div className="cd-component__dropdown">
+      <div
+        className="cd-component__dropdown-trigger"
+        tabIndex={0}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="cd-component__dropdown-selection">
+          {selectedItem ? selectedItem.text : "Select an item..."}
+        </span>
+      </div>
+
+      {isOpen && (
+        <div className="cd-component__dropdown-menu">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              onClick={() => setSelectedItem(item)}
+              className="cd-component__dropdown-item"
+            >
+              <div className="cd-component__dropdown-item--details">
+                {/* TODO: Add image */}
+                <div>{item.text}</div>
+                <small>{item.description}</small>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export { Dropdown };
+export type { DropdownItem };
