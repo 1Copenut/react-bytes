33e7ba6454801f6dd56a4b025f4612d469b3ae47
1Copenut <trevor.pierce@elastic.co>
2023-12-11 12:08:19 -0600

    Basic keyboard navigation logic added.

--- headless-dropdown-component/src/Components/Dropdown/Dropdown.tsx
+++ headless-dropdown-component/src/Components/Dropdown/Dropdown.tsx
@@ -6,14 +6,54 @@ import { DropdownMenu, DropdownItem, DropdownProps } from "./DropdownMenu";
 const Dropdown = ({ dropdownItems }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState<DropdownItem | null>(null);
+  const [selectedIndex, setSelectedIndex] = useState<number>(-1);
 
   const handleDropdownItemClick = (currentItem: DropdownItem) => {
     setSelectedItem(currentItem);
     setIsOpen(false);
   };
 
+  const handleKeyDown = (e: React.KeyboardEvent) => {
+    switch (e.key) {
+      case "Enter":
+      case " ":
+        if (selectedIndex !== -1) {
+          setSelectedItem(dropdownItems[selectedIndex]);
+        }
+        setIsOpen(!isOpen);
+        break;
+      case "ArrowDown":
+      case "ArrowRight":
+        if (isOpen !== true) {
+          setIsOpen(true);
+        }
+        if (
+          selectedIndex === -1 ||
+          selectedIndex === dropdownItems.length - 1
+        ) {
+          setSelectedIndex(0);
+        } else {
+          setSelectedIndex(selectedIndex + 1);
+        }
+        break;
+      case "ArrowUp":
+      case "ArrowLeft":
+        if (isOpen !== true) {
+          setIsOpen(true);
+        }
+        if (selectedIndex === -1 || selectedIndex === 0) {
+          setSelectedIndex(dropdownItems.length - 1);
+        } else {
+          setSelectedIndex(selectedIndex - 1);
+        }
+        break;
+      default:
+        null;
+    }
+  };
+
   return (
-    <div className="cd-component__dropdown">
+    <div className="cd-component__dropdown" onKeyDown={handleKeyDown}>
       <DropdownTrigger
         onClick={() => setIsOpen(!isOpen)}
         selection={selectedItem ? selectedItem.text : "Select an item..."}
