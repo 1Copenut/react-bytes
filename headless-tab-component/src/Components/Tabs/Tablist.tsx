cf39d8ff00d50caf010933f26dcfe9b0b2d8fa86
1Copenut <pierce.trevor@gmail.com>
2023-12-23 13:43:57 -0600

    Splitting tab rendering to separate function before splitting components.

--- headless-tab-component/src/Components/Tabs/Tablist.tsx
+++ headless-tab-component/src/Components/Tabs/Tablist.tsx
@@ -1,24 +1,56 @@
+import { ReactElement, useState } from "react";
 import "./Tabs.css";
 
+interface Tab {
+  title: string;
+  content: string;
+}
+
+const tabData: Tab[] = [
+  {
+    title: "Tab 1 title",
+    content:
+      "This is the content for tab number one. Pretty basic at this point.",
+  },
+  {
+    title: "Tab 2 title",
+    content:
+      "This is the content for tab number two. Pretty basic at this point.",
+  },
+  {
+    title: "Tab 3 title",
+    content:
+      "This is the content for tab number three. Pretty basic at this point.",
+  },
+];
+
+const renderTabs = (currentTabIndex: number): ReactElement[] => {
+  return tabData.map((tab, index) => {
+    const { title } = tab;
+    const tabClass = index === currentTabIndex ? "js__tab--current" : undefined;
+
+    return (
+      <li role="presentation" key={index}>
+        <a
+          className={tabClass}
+          href={`#tab-${index}`}
+          id={`tab-link-${index}`}
+          role="tab"
+        >
+          {title}
+        </a>
+      </li>
+    );
+  });
+};
+
 const Tablist = () => {
+  const [currentTab, setCurrentTab] = useState<number>(0);
+
   return (
     <>
       <ul role="tablist" className="cd-component__tablist">
-        <li role="presentation">
-          <a href="#tab-1" id="tab-link-1" role="tab">
-            Tab 1 title
-          </a>
-        </li>
-        <li role="presentation">
-          <a href="#tab-2" id="tab-link-2" role="tab">
-            Tab 2 title
-          </a>
-        </li>
-        <li role="presentation">
-          <a href="#tab-3" id="tab-link-3" role="tab">
-            Tab 3 title
-          </a>
-        </li>
+        {renderTabs(currentTab)}
       </ul>
       <div aria-labelledby="tab-link-1" id="tab-1" role="tabpanel" tabIndex={0}>
         This is the content for tab number one. Pretty basic at this point.
